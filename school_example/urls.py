6a9e4e96e39925d841ce7ec2d757feb2196affd4
CanekTorralba <a6519150036@utch.edu.mx>
2021-06-07 21:57:56 -0600

    _message, landing, login and logout views

--- school_example/urls.py
+++ school_example/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from users import views as user_views
+from users import views as users_views
 
 #   Static files config
 from django.conf import settings
@@ -8,7 +8,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', user_views.login, name='login'),
-    path('new/', user_views.signup, name='signup')
+    path('', users_views.login_view, name='login'),
+    path('new/', users_views.signup, name='signup'),
+    path("landing/", users_views.landing, name='landing'),
+    path("logout/", users_views.logout_view, name="logout")
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,8 +1,45 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+
+#Views
+from django.views import View
+
+#Models
+from django.contrib.auth.models import User
 
 # Create your views here.
 def login(request):
+    """ Login View """
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            return redirect('landing')
+        else:
+            try:
+                user = User.objects.get(username=username)
+                return render(request, 'users/login.html',{'error':'Invalid password'})
+            except:
+                return render(request, 'users/login.html',{'error':'Invalid username and password'})
+
+    
+    
     return render(request, 'users/login.html')
 
 def signup(request):
     return render(request, 'users/signup.html')
+
+@login_required
+def landing(request):
+    return render(request,'landing.html')
+
+@login_required
+def logout_view(request):
+    """ Logout an user view """
+    logout(request)
+    return redirect('login')
